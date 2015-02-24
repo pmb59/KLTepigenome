efc0a83bb1b93a9d83c73248a655f0c82294f81d
pmb59 <dnaseiseq@gmail.com>
2015-02-24 00:27:07 +0000

    Update KLTepigenome.r

--- KLTepigenome.r
+++ KLTepigenome.r
@@ -1,2 +1,32 @@
-#Madrigal P, Krajewski P: Uncovering the variation within epigenomic datasets using the Karhunen-Loeve transform
+#R script to perform functional Karhunen-Loeve Transform (KLT) in epigenomic data
+#If you use this, please cite:
+#Uncovering the variation within epigenomic datasets using the Karhunen-Loeve transform
+
+args<-commandArgs(TRUE)
+library(fda)
+library(ggplot2)
+
+load("TSS.Rda")
+
+#pdf(file="all.pdf")
+
+#FILE="hg19.100way.phastCons.bw"
+FILE=as.character( read.table("01_data.txt", head=F)$V1 )
+
+FILE=FILE[as.numeric(args[1])]
+
+
+#Prepare regions
+
+EXT = 1500 # bp
+NB = 150
+
+START <- centre - EXT
+END <- centre + EXT  
+#STRAND=strand(x)    
+
+# Check integrity of the regions
+chromos <- read.table("hg19.txt",head=F)
+
+
 
