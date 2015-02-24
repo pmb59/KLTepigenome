8ed15783b062983c6b6129b2cf12a8e202d670fb
pmb59 <dnaseiseq@gmail.com>
2015-02-24 00:35:13 +0000

    Update KLTepigenome.r

--- KLTepigenome.r
+++ KLTepigenome.r
@@ -11,14 +11,13 @@ load("TSS.Rda")
 #pdf(file="all.pdf")
 
 #FILE="hg19.100way.phastCons.bw"
-FILE=as.character( read.table("01_data.txt", head=F)$V1 )
-
-FILE=FILE[as.numeric(args[1])]
+FILE=as.character(args[1])
 
 
 #Prepare regions
-
+#Extension
 EXT = 1500 # bp
+#Number of B-Splines
 NB = 150
 
 START <- centre - EXT
@@ -26,7 +25,39 @@ END <- centre + EXT
 #STRAND=strand(x)    
 
 # Check integrity of the regions
-chromos <- read.table("hg19.txt",head=F)
+chromos <- read.table("hg19.chrom.sizes",head=F)
+
+valid <- c()
+for (k in 1:length(CHR)){
+
+        locata <- which( chromos$V1 == CHR[k] )
+        if (length(locata)==1){                    #e.g. chrGL000213.1
+        	if (as.integer(START[k])>0  & as.integer(END[k]) < chromos$V2[locata] ) { valid <- c(valid,k) }
+        }
+
+}
+
+START <- START[valid]
+END <- END[valid]
+CHR <- CHR[valid]
+STRAND <- STRAND[valid]
+
+#OK
+
+# REMOVE ENCODE Blacklisted regions
+#(do this in temp folder)
+
+write.table(cbind(CHR,START,END,STRAND), file = "original.bed", append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+system('bedtools intersect -wa -v -a original.bed -b wgEncodeDacMapabilityConsensusExcludable.bed  > intersect.bed', intern=FALSE);
+NEWbed <- read.table("intersect.bed",head=F)
+START <-  NEWbed[,2]
+END <-   NEWbed[,3]
+CHR <- NEWbed[,1]
+STRAND <-  NEWbed[,4]
+system('rm original.bed', intern=FALSE);
+system('rm intersect.bed', intern=FALSE);
+
+for (j in
 
 
 
